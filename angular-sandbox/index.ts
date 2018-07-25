7d73d1dd6cefac9c829a8c9ec39e160b88662a8e
Victoria Brown <victoriabrown0710@gmail.com>
2018-07-25 10:31:45 -0500

    Update index.ts

--- angular-sandbox/index.ts
+++ angular-sandbox/index.ts
@@ -206,3 +206,28 @@ function identity<T>(argument: T): string {
 }
 
 console.log(identity(42));
+
+// Yay for decorators!!
+function exampleDecorator(target: Object, propertyKey: string, descriptor: PropertyDescriptor): any {
+    let oldValue = descriptor.value;
+
+    descriptor.value = function() {
+        console.log(`Calling ${propertyKey} with `, target);
+        let value = oldValue.apply(null, [arguments[1], arguments[0]]);
+
+        console.log(`Function is executed`);
+        return value + '; Decorators are crazy!';
+    };
+
+    return descriptor;
+}
+
+class MyFunction { 
+    @exampleDecorator
+    exampleFunction(arg1: string, arg2: string) {
+        console.log(`Arguments Received: ${arg1} and ${arg2}`);
+        return `${arg1} ${arg2}`;
+    }
+}
+const run = new MyFunction();
+console.log(run.exampleFunction('Hello', 'World'));
