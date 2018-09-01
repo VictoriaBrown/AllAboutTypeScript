9d5acb7c2a56eb8ca1c3d62909a2d5332cc3a030
Victoria Brown <victoriabrown0710@gmail.com>
2018-09-01 08:50:03 -0500

    multiple updates

--- Portfolio-Project/src/app/app-routing.module.ts
+++ Portfolio-Project/src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { ContactFormComponent } from './components/contact-form/contact-form.component';
 import { GreetingComponent } from './components/greeting/greeting.component';
 import { SkillsComponent } from './modules/skills/components/skills/skills.component';
+import { DisplayMoviesComponent } from './modules/movies/display-movies/display-movies.component';
+import { DisplayMusicComponent } from './modules/music/display-music/display-music.component';
 
 const routes: Routes = [
   {
@@ -16,6 +18,14 @@ const routes: Routes = [
   {
     path: 'greeting',
     component: GreetingComponent
+  },
+  {
+    path: 'movies',
+    component: DisplayMoviesComponent
+  },
+  {
+    path: 'music',
+    component: DisplayMusicComponent
   },
   {
     path: '',
--- Portfolio-Project/src/app/app.module.ts
+++ Portfolio-Project/src/app/app.module.ts
@@ -4,13 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { SkillsModule } from './modules/skills/skills.module';
 import { AppComponent } from './app.component';
 import { ContactsModule } from './modules/contacts/contacts.module';
-
 import { SkillsService } from './services/skills.service';
 import { ContactServiceService } from './services/contact-service.service'
 import { ContactsComponentComponent } from './modules/contacts/components/contacts/contacts-component.component';
 import { GreetingComponent } from './components/greeting/greeting.component';
 import { ContactFormComponent } from './components/contact-form/contact-form.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { MoviesModule } from './modules/movies/movies.module';
+import { MusicModule } from './modules/music/music.module';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,9 @@ import { AppRoutingModule } from './/app-routing.module';
     FormsModule,
     SkillsModule,
     ContactsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MoviesModule,
+    MusicModule
   ],
   providers: [SkillsService],
   bootstrap: [AppComponent]
